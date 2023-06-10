87cb7a44212f6395531cc633a4bdc0fe41a48d1e
rakibul58 <rhrahi14@gmail.com>
2023-06-10 22:36:11 +0600

    meet the masters

--- src/Pages/Home/MeetTheMasters/MeetTheMaster.js
+++ src/Pages/Home/MeetTheMasters/MeetTheMaster.js
@@ -8,6 +8,16 @@ import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRou
 import './style.css';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import guptaImg from '../../../assets/Masters/guptaImg.png';
+import guptaLogo from '../../../assets/Masters/guptaLogo.png';
+import handaImg from '../../../assets/Masters/handaImg.png';
+import handaLogo from '../../../assets/Masters/handaLogo.png';
+import jainImg from '../../../assets/Masters/jainImg.png';
+import jainLogo from '../../../assets/Masters/jainLogo.png';
+import gargImg from '../../../assets/Masters/gargImg.png';
+import gargLogo from '../../../assets/Masters/gargLogo.png';
+import moulikImg from '../../../assets/Masters/moulikImg.png';
+import moulikLogo from '../../../assets/Masters/moulikLogo.png';
 
 const MeetTheMaster = () => {
 
@@ -25,152 +35,119 @@ const MeetTheMaster = () => {
         <div className='mt-44 px-10 lg:px-28'>
             <div className='mb-10 flex justify-between items-start'>
                 <div>
-                    <h1 className='text-2xl xl:text-4xl font-extrabold font'>Learn from the <span>Masters, Hands-On</span></h1>
+                    <h1 className='text-3xl xl:text-4xl font-extrabold font'>Meet Your MENTORS </h1>
                     <p className='text-xl mt-2 font-thin lowercase'>Take a closer look into our classrooms
                     </p>
                 </div>
                 <div className='hidden lg:block'>
-                    <Button endIcon={<ArrowForwardIosIcon />} size='large' variant='contained' sx={{ bgcolor: '#94A4FF', ":hover": { bgcolor: '#6278FF' }, textTransform: 'capitalize', borderRadius:'24px' }}>Meet the Masters</Button>
+                    <Button endIcon={<ArrowForwardIosIcon />} size='large' variant='contained' sx={{ bgcolor: '#94A4FF', ":hover": { bgcolor: '#6278FF' }, textTransform: 'capitalize', borderRadius: '24px' }}>Meet the Masters</Button>
                 </div>
             </div>
-            <div
-                ref={containerRef}
-                className='flex overflow-x-scroll scroll-smooth gap-5 mtm-container'
-            >
-                <div className='border-2 border-gray-400 bg-[#424242] hover:shadow-xl hover:transition-all hover:duration-200 hover:ease-out hover:shadow-custom-blue min-w-[290px] max-w-[290px]'>
-                    <iframe
-                        className='w-full h-[210px] object-cover'
-                        src={`https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1290096-NRkICuxxxg-high.mp4`}
-                        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                    <div className='p-3'>
-                        <h4 className='font-extrabold font'>Full Name</h4>
-                        <h6 className='text-sm font-thin'>Work Role, Company</h6>
-                        <p className='text-sm font-thin mt-2'>Teaching :</p>
-                        <p className='text-xs font-semibold'>Full Subject Name</p>
-                    </div>
-                </div>
-                <div className='border-2 border-gray-400 bg-[#424242] hover:shadow-xl hover:transition-all hover:duration-200 hover:ease-out hover:shadow-custom-blue min-w-[290px] max-w-[290px]'>
-                    <iframe
-                        className='w-full h-[210px] object-cover'
-                        src={`https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1290096-NRkICuxxxg-high.mp4`}
-                        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                    <div className='p-3'>
-                        <h4 className='font-extrabold font'>Full Name</h4>
-                        <h6 className='text-sm font-thin'>Work Role, Company</h6>
-                        <p className='text-sm font-thin mt-2'>Teaching :</p>
-                        <p className='text-xs font-semibold'>Full Subject Name</p>
-                    </div>
-                </div>
-                <div className='border-2 border-gray-400 bg-[#424242] hover:shadow-xl hover:transition-all hover:duration-200 hover:ease-out hover:shadow-custom-blue min-w-[290px] max-w-[290px]'>
-                    <iframe
-                        className='w-full h-[210px] object-cover'
-                        src={`https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1290096-NRkICuxxxg-high.mp4`}
-                        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                    <div className='p-3'>
-                        <h4 className='font-extrabold font'>Full Name</h4>
-                        <h6 className='text-sm font-thin'>Work Role, Company</h6>
-                        <p className='text-sm font-thin mt-2'>Teaching :</p>
-                        <p className='text-xs font-semibold'>Full Subject Name</p>
-                    </div>
-                </div>
-                <div className='border-2 border-gray-400 bg-[#424242] hover:shadow-xl hover:transition-all hover:duration-200 hover:ease-out hover:shadow-custom-blue min-w-[290px] max-w-[290px]'>
-                    <iframe
-                        className='w-full h-[210px] object-cover'
-                        src={`https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1290096-NRkICuxxxg-high.mp4`}
-                        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                    <div className='p-3'>
-                        <h4 className='font-extrabold font'>Full Name</h4>
-                        <h6 className='text-sm font-thin'>Work Role, Company</h6>
-                        <p className='text-sm font-thin mt-2'>Teaching :</p>
-                        <p className='text-xs font-semibold'>Full Subject Name</p>
-                    </div>
-                </div>
-                <div className='border-2 border-gray-400 bg-[#424242] hover:shadow-xl hover:transition-all hover:duration-200 hover:ease-out hover:shadow-custom-blue min-w-[290px] max-w-[290px]'>
-                    <iframe
-                        className='w-full h-[210px] object-cover'
-                        src={`https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1290096-NRkICuxxxg-high.mp4`}
-                        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                    <div className='p-3'>
-                        <h4 className='font-extrabold font'>Full Name</h4>
-                        <h6 className='text-sm font-thin'>Work Role, Company</h6>
-                        <p className='text-sm font-thin mt-2'>Teaching :</p>
-                        <p className='text-xs font-semibold'>Full Subject Name</p>
-                    </div>
-                </div>
-                <div className='border-2 border-gray-400 bg-[#424242] hover:shadow-xl hover:transition-all hover:duration-200 hover:ease-out hover:shadow-custom-blue min-w-[290px] max-w-[290px]'>
-                    <iframe
-                        className='w-full h-[210px] object-cover'
-                        src={`https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1290096-NRkICuxxxg-high.mp4`}
-                        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                    <div className='p-3'>
-                        <h4 className='font-extrabold font'>Full Name</h4>
-                        <h6 className='text-sm font-thin'>Work Role, Company</h6>
-                        <p className='text-sm font-thin mt-2'>Teaching :</p>
-                        <p className='text-xs font-semibold'>Full Subject Name</p>
-                    </div>
-                </div>
-                <div className='border-2 border-gray-400 bg-[#424242] hover:shadow-xl hover:transition-all hover:duration-200 hover:ease-out hover:shadow-custom-blue min-w-[290px] max-w-[290px]'>
-                    <iframe
-                        className='w-full h-[210px] object-cover'
-                        src={`https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1290096-NRkICuxxxg-high.mp4`}
-                        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                    <div className='p-3'>
-                        <h4 className='font-extrabold font'>Full Name</h4>
-                        <h6 className='text-sm font-thin'>Work Role, Company</h6>
-                        <p className='text-sm font-thin mt-2'>Teaching :</p>
-                        <p className='text-xs font-semibold'>Full Subject Name</p>
-                    </div>
-                </div>
-                <div className='border-2 border-gray-400 bg-[#424242] hover:shadow-xl hover:transition-all hover:duration-200 hover:ease-out hover:shadow-custom-blue min-w-[290px] max-w-[290px]'>
-                    <iframe
-                        className='w-full h-[210px] object-cover'
-                        src={`https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1290096-NRkICuxxxg-high.mp4`}
-                        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                    <div className='p-3'>
-                        <h4 className='font-extrabold font'>Full Name</h4>
-                        <h6 className='text-sm font-thin'>Work Role, Company</h6>
-                        <p className='text-sm font-thin mt-2'>Teaching :</p>
-                        <p className='text-xs font-semibold'>Full Subject Name</p>
-                    </div>
-                </div>
 
-            </div>
             <div className="carousel-button-group">
-                <div className='flex justify-left mt-5'>
+                <div className='flex justify-center gap-10 lg:mt-5'>
                     <button onClick={handleScrollLeft} className='hidden lg:block' type="button">
-                        <ArrowBackIosNewIcon className='opacity-80' sx={{ fontSize: '50px', color: 'rgb(156 163 175)', ":hover": { color: "#397FEB" } }} />
+                        <ArrowBackIosNewIcon sx={{ fontSize: '28px', color: '#141414', borderRadius: '50%', ":hover": { color: "#397FEB" }, background: '#94A4FF', height: '40px', width: '40px', padding: '5px' }} />
                     </button>
+                    <div
+                        ref={containerRef}
+                        className='flex overflow-x-scroll scroll-smooth gap-5 mtm-container lg:w-[85vw]'
+                    >
+                        <div style={{
+                            backgroundColor: '#FFFFFF',
+                            borderBottom: '4px solid #94A4FF',
+                            borderRight: '4px solid #94A4FF',
+                            color: 'black',
+                            borderRadius: '24px'
+                        }} className='min-w-[290px] max-w-[290px] px-2 pb-4'>
+                            <img src={guptaImg} alt="" />
+                            <div className='pt-3'>
+                                <h4 className='font-bold text-xl'>Shekhar gupta</h4>
+                                <h6 className='text-sm font-normal'>Former worked at</h6>
+                            </div>
+                            <img src={guptaLogo} alt="" />
+                        </div>
+
+
+                        <div style={{
+                            backgroundColor: '#FFFFFF',
+                            borderBottom: '4px solid #94A4FF',
+                            borderRight: '4px solid #94A4FF',
+                            color: 'black',
+                            borderRadius: '24px'
+                        }} className='min-w-[290px] max-w-[290px] px-2 pb-4'>
+                            <img src={handaImg} alt="" />
+                            <div className='pt-3'>
+                                <h4 className='font-bold text-xl'>Pulkit Handa</h4>
+                                <h6 className='text-sm font-normal'>Former worked at</h6>
+                            </div>
+                            <img src={handaLogo} alt="" />
+                        </div>
+
+
+
+                        <div style={{
+                            backgroundColor: '#FFFFFF',
+                            borderBottom: '4px solid #94A4FF',
+                            borderRight: '4px solid #94A4FF',
+                            color: 'black',
+                            borderRadius: '24px'
+                        }} className='min-w-[290px] max-w-[290px] px-2 pb-4'>
+                            <img src={jainImg} alt="" />
+                            <div className='py-3'>
+                                <h4 className='font-bold text-xl'>Naman Jain</h4>
+                                <h6 className='text-sm font-normal'>Former director of Classplas
+                                    dineout, Times Internet</h6>
+                            </div>
+                            <img src={jainLogo} alt="" />
+                        </div>
+
+
+
+                        <div style={{
+                            backgroundColor: '#FFFFFF',
+                            borderBottom: '4px solid #94A4FF',
+                            borderRight: '4px solid #94A4FF',
+                            color: 'black',
+                            borderRadius: '24px'
+                        }} className='min-w-[290px] max-w-[290px] px-2 pb-4'>
+                            <img src={gargImg} alt="" />
+                            <div className='py-3'>
+                                <h4 className='font-bold text-xl'>Aayush Garg</h4>
+                                <h6 className='text-sm font-normal'>Former worked at</h6>
+                            </div>
+                            <img src={gargLogo} alt="" />
+                        </div>
+
+
+
+                        <div style={{
+                            backgroundColor: '#FFFFFF',
+                            borderBottom: '4px solid #94A4FF',
+                            borderRight: '4px solid #94A4FF',
+                            color: 'black',
+                            borderRadius: '24px'
+                        }} className='min-w-[290px] max-w-[290px] px-2 pb-4'>
+                            <img src={moulikImg} alt="" />
+                            <div className='py-3'>
+                                <h4 className='font-bold text-xl'>Siddharth Moulik</h4>
+                                <h6 className='text-sm font-normal'>Former worked at</h6>
+                            </div>
+                            <img src={moulikLogo} alt="" />
+                        </div>
+
+
+
+
+
+                    </div>
                     <button onClick={handleScrollRight} className='hidden lg:block' type="button">
-                        <ArrowBackIosNewIcon className='opacity-80' sx={{ fontSize: '50px', color: '#397FEB', ":hover": { color: "#397FEB" } }} />
+                        <ArrowForwardIosIcon sx={{ fontSize: '28px', color: '#141414', borderRadius: '50%', ":hover": { color: "#397FEB" }, background: '#94A4FF', height: '40px', width: '40px', padding: '5px' }} />
                     </button>
                 </div>
             </div>
             <div className='block lg:hidden mt-10'>
-                <Button endIcon={<OutboundOutlinedIcon />} size='large' variant='contained' sx={{ bgcolor: '#397FEB', ":hover": { bgcolor: '#0CC5DB' }, textTransform: 'capitalize', width: '100%' }}>Meet the Masters</Button>
+                <Button endIcon={<ArrowForwardIosIcon />} size='large' variant='contained' sx={{ bgcolor: '#94A4FF', ":hover": { bgcolor: '#6278FF' }, textTransform: 'capitalize', borderRadius: '24px', width: '100%' }}>Meet the Masters</Button>
             </div>
         </div>
     );
