0cd7b399df0b80f52ab08c70c6380aa230bbfce9
sohan <tanvirsohaniiuc@gmail.com>
2023-11-24 15:33:18 +0600

    update

--- src/Pages/Template/ExperimentLabsTemplate/Home/LifeAtUnion/LifeAtUnion.js
+++ src/Pages/Template/ExperimentLabsTemplate/Home/LifeAtUnion/LifeAtUnion.js
@@ -1,29 +1,35 @@
 import InstagramIcon from '@mui/icons-material/Instagram';
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 //import collan from '../../../assets/collan.png';
 
-const LifeAtUnion = ({lifeAtUnionData}) => {
+const LifeAtUnion = ({ lifeAtUnionData }) => {
+    const [reviews, setReviews] = useState([...lifeAtUnionData?.reviews]);
     return (
         <div className='mt-40 pb-20 px-10 lg:px-28'>
             <div className='flex items-start justify-between gap-10'>
                 <div>
-                    <h1 className='text-3xl font-semibold'>People Speak @  Experiment Labs</h1>
+                    <h1 className='text-3xl font-semibold'>People Speak @  {lifeAtUnionData?.organizationName}</h1>
                 </div>
             </div>
             <div className='mt-12 flex justify-between items-end gap-10 overflow-x-scroll scroll-smooth my-container'>
-                <div>
-                    <iframe
-                        width="290"
-                        height="480"
-                        className='rounded-3xl'
-                        src={`https://www.youtube.com/embed/lKkFKWQvaLs`}
-                        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                </div>
-                <div>
+                {
+                    reviews?.map((review) => (
+                        <div>
+                            <iframe
+                                width="290"
+                                height="480"
+                                className='rounded-3xl'
+                                src={review?.videoLink}
+                                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                title="Embedded youtube"
+                            />
+                        </div>
+                    ))
+                }
+
+                {/*   <div>
                     <iframe
                         width="290"
                         height="480"
@@ -44,8 +50,8 @@ const LifeAtUnion = ({lifeAtUnionData}) => {
                         allowFullScreen
                         title="Embedded youtube"
                     />
-                </div>
-                <img className='lg:block hidden' style={{height:'170px'}} src={lifeAtUnionData?.collan} alt="" />
+                </div> */}
+                <img className='lg:block hidden' style={{ height: '170px' }} src={lifeAtUnionData?.image} alt="" />
             </div>
         </div>
     );
