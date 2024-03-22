a0926349bdb743e5e991d3a98e54bc5224c8c060
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2024-03-22 11:04:56 +0600

    sohan

--- src/Pages/Dashboard/UpdateOrganization/LanguageSetting/AddLearnersDetails.js
+++ src/Pages/Dashboard/UpdateOrganization/LanguageSetting/AddLearnersDetails.js
@@ -63,6 +63,23 @@ const AddLearnersDetails = () => {
     const studentStatus = form.studentStatus?.value;
     const free = form.free?.value;
     const paid = form.paid?.value;
+    const noCourseAddedYet = form.noCourseAddedYet?.value;
+    const selectBatch = form.selectBatch?.value;
+    const noBatchAddedYet = form.noBatchAddedYet?.value;
+    const file = form.file?.value;
+    const enterPhoneNumber = form.enterPhoneNumber?.value;
+    const newUsersCreatedSuccessfully = form.newUsersCreatedSuccessfully?.value;
+    const uploadFile = form.uploadFile?.value;
+    const dragAndDrop = form.dragAndDrop?.value;
+    const or = form.or?.value;
+    const browser = form.browser?.value;
+    const downloadTemplate = form.downloadTemplate?.value;
+    const name = form.name?.value;
+    const email = form.email?.value;
+    const phone = form.phone?.value;
+    const addStudents = form.addStudents?.value;
+    const newUserCreatedSuccessfully = form.newUserCreatedSuccessfully?.value;
+    const pleaseFollowTheGivenTemplateOfXlsxFileToEnterStudentData = form.pleaseFollowTheGivenTemplateOfXlsxFileToEnterStudentData?.value;
 
 
     const itemDetail = {
@@ -76,6 +93,23 @@ const AddLearnersDetails = () => {
       studentStatus: studentStatus,
       free: free,
       paid: paid,
+      noCourseAddedYet: noCourseAddedYet,
+      selectBatch: selectBatch,
+      noBatchAddedYet: noBatchAddedYet,
+      file: file,
+      enterPhoneNumber: enterPhoneNumber,
+      newUsersCreatedSuccessfully: newUsersCreatedSuccessfully,
+      uploadFile: uploadFile,
+      dragAndDrop: dragAndDrop,
+      or: or,
+      browser: browser,
+      pleaseFollowTheGivenTemplateOfXlsxFileToEnterStudentData: pleaseFollowTheGivenTemplateOfXlsxFileToEnterStudentData,
+      downloadTemplate: downloadTemplate,
+      name: name,
+      email: email,
+      phone: phone,
+      addStudents: addStudents,
+      newUserCreatedSuccessfully: newUserCreatedSuccessfully,
 
 
     };
@@ -144,6 +178,74 @@ const AddLearnersDetails = () => {
               <div className='flex justify-between items-center w-[100%]'>
                 <p className='text-lg font-medium'>Paid</p>
                 <input name='paid' defaultValue={itemDetails?.paid} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>No course added yet</p>
+                <input name='noCourseAddedYet' defaultValue={itemDetails?.noCourseAddedYet} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Select Batch</p>
+                <input name='selectBatch' defaultValue={itemDetails?.selectBatch} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>No batch added yet</p>
+                <input name='noBatchAddedYet' defaultValue={itemDetails?.noBatchAddedYet} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>File</p>
+                <input name='file' defaultValue={itemDetails?.file} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Enter phone number</p>
+                <input name='enterPhoneNumber' defaultValue={itemDetails?.enterPhoneNumber} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>New Users created successfully</p>
+                <input name='newUsersCreatedSuccessfully' defaultValue={itemDetails?.newUsersCreatedSuccessfully} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Upload File</p>
+                <input name='uploadFile' defaultValue={itemDetails?.uploadFile} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Drag and drop</p>
+                <input name='dragAndDrop' defaultValue={itemDetails?.dragAndDrop} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Or</p>
+                <input name='or' defaultValue={itemDetails?.or} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Browser</p>
+                <input name='browser' defaultValue={itemDetails?.browser} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Please follow the given template of xlsx file to enter student data</p>
+                <input name='pleaseFollowTheGivenTemplateOfXlsxFileToEnterStudentData' defaultValue={itemDetails?.pleaseFollowTheGivenTemplateOfXlsxFileToEnterStudentData} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Download Template</p>
+                <input name='downloadTemplate' defaultValue={itemDetails?.downloadTemplate} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Name</p>
+                <input name='name' defaultValue={itemDetails?.name} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Email</p>
+                <input name='email' defaultValue={itemDetails?.email} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Phone</p>
+                <input name='phone' defaultValue={itemDetails?.phone} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>Add Students</p>
+                <input name='addStudents' defaultValue={itemDetails?.addStudents} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
+              </div>
+              <div className='flex justify-between items-center w-[100%]'>
+                <p className='text-lg font-medium'>New User created successfully</p>
+                <input name='newUserCreatedSuccessfully' defaultValue={itemDetails?.newUserCreatedSuccessfully} type='text' className='border border-slate-300 rounded-lg p-2 w-[300px]' placeholder='Text here' />
               </div>
 
             </div>
